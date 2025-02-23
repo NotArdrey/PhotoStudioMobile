f6e4518df80f89a3e3e0fb1157cd08566237b26e
brent <brentreed623@gmail.com>
2025-02-23 21:02:34 +0800

    Added icons and backbuttos

---
+++ app/src/main/java/com/example/photostudio/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.photostudio
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen) // Make sure this matches your XML filename
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LandingPage::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
+    }
+}
+
